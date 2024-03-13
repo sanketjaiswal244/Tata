d056501a31f88f564600ad757e2778e2142e1b0e
Sanket Jaiswal <sanketjaiswal244@gmail.com>
2024-03-13 16:01:58 +0530

    2nd feature commt

--- Sum/src/temp/Arithmatic.java
+++ Sum/src/temp/Arithmatic.java
@@ -5,5 +5,6 @@ public class Arithmatic {
 	public static void main(String[] args) {
 		System.out.println("1st from eclipse");
 		System.out.println("conflic line from feature branch-1");
+		System.out.println("conflic 2nd line from feature branch-1");
 	}
 }
