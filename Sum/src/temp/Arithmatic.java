cd8c75fd5ad5e5dcc315bdd6e379dfe9d27d7c5f
Sanket Jaiswal <sanketjaiswal244@gmail.com>
2024-03-13 16:29:07 +0530

    feture confildt commit

--- Sum/src/temp/Arithmatic.java
+++ Sum/src/temp/Arithmatic.java
@@ -5,5 +5,6 @@ public class Arithmatic {
 	public static void main(String[] args) {
 		System.out.println("1st from eclipse");
 		System.out.println("1st from Github");
+		System.out.println("conflict from feature");
 	}
 }
